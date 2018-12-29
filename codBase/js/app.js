91fe88f1ed0a3f7a1b829e73ff86d4621a11b84a
jlinarez <jr.linarez@gmail.com>
2018-12-29 00:05:47 -0400

    version 0.1
    contiene todas la funcionalidades de la las teclas

--- codBase/js/app.js
+++ codBase/js/app.js
@@ -1,5 +1,8 @@
 var Calculadora = {
   operador : "",
+  valorA : 0.00,
+  valorB : 0.00,
+  inicializar : true,
 
   //Trunca la pantalla a solo 8 digitos
   truncaCadena: function(cadena){
@@ -13,12 +16,16 @@ var Calculadora = {
 
   //Cambia el signo del valor en pantalla
   signo: function(cadena){
-    retorno = parseFloat(cadena) * -1
-    Calculadora.imprimePantalla(retorno.toString())
+    if(cadena != "0"){
+      console.log(cadena)
+      retorno = parseFloat(cadena) * -1
+      console.log(retorno)
+      document.getElementById('display').innerText = retorno
+    }
   },
 
   //Imprime el resultado en la pantalla
-  imprimePantalla: function(elemento){
+  imprimeDigito: function(elemento){
       var pantalla = document.getElementById('display')
       if(pantalla.innerText.indexOf("0") === 0 && elemento === "0" && pantalla.innerText.indexOf(".") != 1){
         pantalla.innerText = "0";
@@ -34,8 +41,34 @@ var Calculadora = {
 
 //      document.getElementById('display').innerText = document.getElementById('display').innerText + elemento
   },
+
+  calculaOperacion : function(operador){
+    Calculadora.valorB = document.getElementById('display').innerText;
+    var resultado
+    switch (operador) {
+      case "+":
+        resultado = parseFloat(Calculadora.valorA) + parseFloat(Calculadora.valorB);
+        break;
+      case "-":
+        resultado = parseFloat(Calculadora.valorA) - parseFloat(Calculadora.valorB);
+        break;
+      case "*":
+        resultado = parseFloat(Calculadora.valorA) * parseFloat(Calculadora.valorB);
+        break;
+      case "/":
+          if(parseFloat(Calculadora.valorB) == 0){
+            resultado = "ERROR";
+            break;
+          }else{
+            resultado = parseFloat(Calculadora.valorA) / parseFloat(Calculadora.valorB);
+            break;
+          }
+    }
+    document.getElementById('display').innerText = Calculadora.truncaCadena(resultado.toString())
+  },
+
   //Funcion asigna eventos a la teclas
-  asignaEventos: function(){
+  ini: function(){
     //Crea animacion al pulsar cada tecla.
     var teclas = document.querySelectorAll(".tecla");
     for (var tecla of teclas) {
@@ -54,7 +87,7 @@ var Calculadora = {
     for(var x = 0; x < 10; x++){
       num[x] = document.getElementById(x)
       num[x].addEventListener('click', function(evt){
-      Calculadora.imprimePantalla(num[evt.target.id].id)
+      Calculadora.imprimeDigito(num[evt.target.id].id)
       })
     }
 
@@ -65,15 +98,46 @@ var Calculadora = {
 
     //Asigna evento a la tecla PUNTO(.)
     document.getElementById('punto').addEventListener('click', function(){
-      Calculadora.imprimePantalla(".")
+      Calculadora.imprimeDigito(".")
     })
 
     //Asigno evento a la tecla(+/-)
-    document.getElementById("sign").addEventListener('click', Calculadora.signo(document.getElementById("display").innerText))
-  },
+    document.getElementById("sign").addEventListener('click', function() {
+      Calculadora.signo(document.getElementById("display").innerText)
+    })
 
-  ini : function(){
-    this.asignaEventos();
+    //Asigna evento a la tecla Suma(+)
+    document.getElementById("mas").addEventListener('click', function() {
+      Calculadora.valorA = document.getElementById("display").innerText;
+      console.log(Calculadora.valorA)
+      Calculadora.operador = "+";
+      document.getElementById("display").innerText = "";
+    })
+
+    //Asigna evento a la tecla Resta(-)
+    document.getElementById("menos").addEventListener('click', function() {
+      Calculadora.valorA = document.getElementById("display").innerText;
+      Calculadora.operador = "-";
+      document.getElementById("display").innerText = "";
+    })
+
+    //Asigna evento a la tecla Multiplicacion](*)
+    document.getElementById("por").addEventListener('click', function() {
+      Calculadora.valorA = document.getElementById("display").innerText;
+      Calculadora.operador = "*";
+      document.getElementById("display").innerText = "";
+    })
+
+    //Asigna evento a la tecla Divicion(/)
+    document.getElementById("dividido").addEventListener('click', function() {
+      Calculadora.valorA = document.getElementById("display").innerText;
+      Calculadora.operador = "/";
+      document.getElementById("display").innerText = "";
+    })
+
+    document.getElementById("igual").addEventListener('click', function(){
+      Calculadora.calculaOperacion(Calculadora.operador)
+    })
   }
 
 } //fin calculadora
